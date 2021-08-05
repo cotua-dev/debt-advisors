0d09394c40ab7b9aa151ecc639e23190800d14e1
Jeffrey Berry <jeffreyberry93@gmail.com>
2021-08-05 09:22:56 -0700

    chore(index.ts): try getting cypress working with nextjs

--- cypress/plugins/index.ts
+++ cypress/plugins/index.ts
@@ -8,6 +8,7 @@
 // You can read more here:
 // https://on.cypress.io/plugins-guide
 // ***********************************************************
+import injectDevServer from '@cypress/react/plugins/next';
 
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
@@ -19,4 +20,6 @@
 module.exports = (on, config) => {
     // `on` is used to hook into various events Cypress emits
     // `config` is the resolved Cypress config
+    injectDevServer(on, config);
+    return config;
 }
