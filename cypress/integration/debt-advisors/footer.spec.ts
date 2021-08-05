a058c89ab586a02bbf0146f1ba2c687529663863
Jeffrey Berry <jeffreyberry93@gmail.com>
2021-08-05 12:12:53 -0700

    test(footer.spec.ts): update footer test

--- cypress/integration/debt-advisors/footer.spec.ts
+++ cypress/integration/debt-advisors/footer.spec.ts
@@ -1,5 +1,15 @@
 describe('Footer', (): void => {
     beforeEach((): Cypress.Chainable<Cypress.AUTWindow> => cy.visit('/'));
 
-    it('displays strong footer text', () => cy.get('footer strong').should('have.text', 'Footer :)'));
+    it('has link to terms of use', (): void => {
+        cy.get('[data-testid=terms-of-use]').should('have.text', 'Terms of Use').click();
+        cy.location('pathname').should('equal', '/terms-of-use');
+        cy.go('back');
+    });
+
+    it('has link to privacy policy', (): void => {
+        cy.get('[data-testid=privacy-policy]').should('have.text', 'Privacy Policy').click();
+        cy.location('pathname').should('equal', '/privacy-policy');
+        cy.go('back');
+    });
 });
