486fbb0c96422a30d17e0e3ebd4644c13e83f2f2
Jeffrey Berry <jeffreyberry93@gmail.com>
2021-08-09 09:17:09 -0700

    wip(CallUsNow): need to add styling

---
+++ components/home/CallUsNow/index.tsx
@@ -0,0 +1,13 @@
+export function CallUsNow(): JSX.Element {
+    return (
+        <section>
+            <div>
+                <h3>{`For expert debt resolution advice you can trust Debt Advisors of America`}</h3>
+                <a href="tel:+18006320437">
+                    <span>{`800-632-0437`}</span>
+                    <span>{`CALL US NOW`}</span>
+                </a>
+            </div>
+        </section>
+    );
+}
