8e82278d4adaec4d2832d4870397d18674e5e2da
Jeffrey Berry <jeffreyberry93@gmail.com>
2021-08-10 21:04:08 -0700

    wip(Verify): add a placeholder component for now

---
+++ components/fields/Verify/index.tsx
@@ -0,0 +1,9 @@
+import styles from './Verify.module.scss';
+
+export function Verify(): JSX.Element {
+    return (
+        <div>
+            <strong>{`Verify`}</strong>
+        </div>
+    );
+}
