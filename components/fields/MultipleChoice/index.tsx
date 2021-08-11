b5b4cf9a2a150720baa90e0c0e9d8393cb9e2367
Jeffrey Berry <jeffreyberry93@gmail.com>
2021-08-10 21:02:52 -0700

    wip(MultipleChoice): add a placeholder component for now

---
+++ components/fields/MultipleChoice/index.tsx
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+import styles from './MultipleChoice.module.scss';
+import { FieldProps } from '../shared.interfaces';
+
+export function MultipleChoiceField(props: FieldProps): JSX.Element {
+    return (
+        <div>
+
+        </div>
+    );
+}
