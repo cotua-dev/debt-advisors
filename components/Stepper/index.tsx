57ad12cf7c5199937a68dcc559a3405fe264f356
Jeffrey Berry <jeffreyberry93@gmail.com>
2021-08-12 09:21:33 -0700

    wip(index.tsx): still need to work on some things like next/previous button

--- components/Stepper/index.tsx
+++ components/Stepper/index.tsx
@@ -1,19 +1,46 @@
 import { useState, useEffect } from 'react';
-import { useRouter } from 'next/router';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleRight, faAngleLeft } from '@fortawesome/free-solid-svg-icons';
 import styles from './Stepper.module.scss';
 import { initialStepperModel, initialSteps, Step, StepperModel } from './Stepper.interfaces';
+import { MultipleChoiceField } from '../fields/MultipleChoice';
 
 export function Stepper(): JSX.Element {
-    const router = useRouter();
     const [model, setModel] = useState(initialStepperModel);
     const [currentStep, setCurrentStep] = useState(0);
     const [steps, setSteps] = useState(initialSteps);
-    const [disableNextButton, setDisableNextButton] = useState(false);
+    const [disableNextButton, setDisableNextButton] = useState(true);
     const [disablePreviousButton, setDisablePreviousButton] = useState(true);
 
+    /**
+     * Initialize the values of every step
+     */
+    /*
+    const initializeStepValues = (): void => {
+        // Initialize variables
+        let step: Step;
+        let property: keyof StepperModel;
+        let updatedSteps: Step[] = [...steps];
+
+        // Loop through all initial steps
+        for (step of updatedSteps) {
+            // Loop through all the properties of each step
+            for (property of step.property) {
+                // Set the initial value to null
+                step.value[property] = null;
+            }
+        }
+
+        // Reset the steps
+        setSteps(updatedSteps);
+    }
+    */
+
+    /*
     useEffect(() => {
+        // Initialize the step values
+        initializeStepValues();
+
         // Get the current model from local storage
         let localStorageModel: string | null = window.localStorage.getItem('model');
 
@@ -24,11 +51,35 @@ export function Stepper(): JSX.Element {
             window.localStorage.setItem('model', stringifiedModel);
             localStorageModel = stringifiedModel;
         } else {
-            // If we got something from local storage, reset the model with the value from local storage
-            setModel(JSON.parse(localStorageModel));
-        }
-    }, []);
+            // Parse the local storage string into the model
+            const parsedLocalStorageModel: StepperModel = JSON.parse(localStorageModel);
 
+            // If we got something from local storage, reset the model with the value from local storage
+            setModel(parsedLocalStorageModel);
+
+            // Update steps using the updated model
+            steps.forEach((step: Step) => {
+                step.property.forEach((property: keyof StepperModel) => {
+                    if (
+                        parsedLocalStorageModel[property] !== null ||
+                        parsedLocalStorageModel[property] !== 0 ||
+                        parsedLocalStorageModel[property] !== ''
+                    ) {
+                        step.value[property] = parsedLocalStorageModel[property];
+                    } else {
+                        step.validity = false;
+                    }
+                });
+            });
+        }
+
+        console.info({steps});
+    }, []);
+    */
+
+    /**
+     * @todo Needs work
+     */
     const previousStep = () => {
         if (currentStep - 1 < 0) {
             setDisablePreviousButton(true);
@@ -41,13 +92,22 @@ export function Stepper(): JSX.Element {
         setDisableNextButton(false);
     }
 
+    /**
+     * @todo Needs work
+     */
     const nextStep = () => {
-        if (currentStep >= steps.length - 1) {
-            setDisableNextButton(true);
-            setCurrentStep(steps.length - 1);
-        } else {
+        if (steps[currentStep].validity) {
             setDisableNextButton(false);
-            setCurrentStep(currentStep + 1);
+
+            if (currentStep >= steps.length - 1) {
+                setDisableNextButton(true);
+                setCurrentStep(steps.length - 1);
+            } else {
+                // setDisableNextButton(false);
+                setCurrentStep(currentStep + 1);
+            }
+        } else {
+            setDisableNextButton(true);
         }
 
         setDisablePreviousButton(false);
@@ -55,6 +115,9 @@ export function Stepper(): JSX.Element {
 
     return (
         <section className={styles['debt-advisors-stepper']}>
+            <div>
+                <small>{`${currentStep + 1} of ${steps.length}`}</small>
+            </div>
             <div className={styles['steps-wrapper']}>
                 {steps.map((step: Step) => {
                     const stepIndex: number = steps.findIndex((thisStep: Step) => thisStep.question === step.question);
@@ -63,6 +126,18 @@ export function Stepper(): JSX.Element {
                     return (
                         <div key={step.question} className={`${styles['step']} ${showStep ? '' : styles['hidden']}`}>
                             <strong className={styles['question']}>{step.question}</strong>
+                            {
+                                step.stepType === 'multiple-choice' &&
+                                    <MultipleChoiceField
+                                        choices={step.choices !== undefined ? step.choices : []}
+                                        field={step.property}
+                                        setModel={setModel}
+                                        model={model}
+                                        nextStep={nextStep}
+                                        steps={steps}
+                                        currentStep={currentStep}
+                                    />
+                            }
                         </div>
                     );
                 })}
