dc3674f54ba9ed8d9acd8ebcfee5523597544623
Jeffrey Berry <jeffreyberry93@gmail.com>
2021-08-13 09:31:02 -0700

    chore(Location.interfaces.ts): add LocationProps interface

---
+++ components/fields/Location/Location.interfaces.ts
@@ -0,0 +1,10 @@
+import { Dispatch, SetStateAction } from 'react';
+import { Step } from '../../Stepper/Stepper.interfaces';
+
+export interface LocationProps {
+    steps: Step[];
+    currentStep: number;
+    setDisableNextButton?: Dispatch<SetStateAction<boolean>>;
+    zipCode: string;
+    setZipCode: Dispatch<SetStateAction<string>>;
+};
