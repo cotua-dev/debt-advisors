b2f65cad52bd891a56f1d35d59ad2cb811f42b97
Jeffrey Berry <jeffreyberry93@gmail.com>
2021-08-16 08:50:37 -0700

    wip(Verify): get field working with react-number-format. Need to add request still

---
+++ components/fields/Verify/Verify.interfaces.ts
@@ -0,0 +1,10 @@
+import { Dispatch, SetStateAction } from 'react';
+import { Step } from '../../Stepper/Stepper.interfaces';
+
+export interface VerifyProps {
+    steps: Step[];
+    currentStep: number;
+    setDisableNextButton: Dispatch<SetStateAction<boolean>>;
+    code: string;
+    setCode: Dispatch<SetStateAction<string>>;
+};
