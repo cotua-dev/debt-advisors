05523b483848f93029fbdc96c8ea1f240fd70490
Jeffrey Berry <jeffreyberry93@gmail.com>
2021-08-17 10:42:22 -0700

    chore(Stepper.enums.ts): add enums for stepper here

---
+++ components/Stepper/Stepper.enums.ts
@@ -0,0 +1,26 @@
+export enum UserPurpose {
+    ReduceYourMonthlyPayments = 69,
+    ConsolidateYourPayments = 71,
+    ReduceYourInterestRates = 73,
+    AllOfTheAbove = 75,
+};
+
+export enum DebtType {
+    CreditCards = 77,
+    PersonalLoans = 79,
+    MedicalBills = 81,
+    AllOfTheAbove = 83,
+};
+
+export enum BehindPaymentsType {
+    YesMore60 = 85,
+    YesMore30 = 87,
+    No = 89,
+};
+
+export enum FallBehindReason {
+    LossOfIncome = 91,
+    IncreasedCostOfLiving = 93,
+    UnexpectedExpenses = 95,
+    MedicalHardship = 97,
+};
