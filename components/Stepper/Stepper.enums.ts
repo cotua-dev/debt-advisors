77a2e64ad0a9efe678d33a0e60e155c12f5f3ca4
Jeffrey Berry <jeffreyberry93@gmail.com>
2021-08-18 09:32:10 -0700

    chore(Stepper.enums.ts): add Questions enum

--- components/Stepper/Stepper.enums.ts
+++ components/Stepper/Stepper.enums.ts
@@ -24,3 +24,17 @@ export enum FallBehindReason {
     UnexpectedExpenses = 95,
     MedicalHardship = 97,
 };
+
+export enum Questions {
+    UserPurpose = 'What are you looking to do?',
+    DebtType = 'What kind of debts do you have?',
+    BehindPaymentsType = 'Are you behind on your payments?',
+    FallBehindReason = 'What made you fall behind?',
+    MonthlyIncomeAmount = 'What is your monthly income?',
+    UnsecuredDebtAmount = 'What is the total amount of your unsecured debt?',
+    ZipCode = 'What city are you in?',
+    Name = 'What is your name?',
+    Email = 'What is your email?',
+    Phone = 'Please provide your phone number for verification',
+    Code = 'Please provide the code sent to your phone number',
+};
