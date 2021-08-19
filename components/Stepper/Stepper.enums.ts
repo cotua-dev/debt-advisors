32ee9a6259c9555a07c21be1754a5a3f4362f674
Jeffrey Berry <jeffreyberry93@gmail.com>
2021-08-19 08:54:11 -0700

    refactor(Stepper.enums.ts): update values

--- components/Stepper/Stepper.enums.ts
+++ components/Stepper/Stepper.enums.ts
@@ -1,28 +1,28 @@
 export enum UserPurpose {
-    ReduceYourMonthlyPayments = 69,
-    ConsolidateYourPayments = 71,
-    ReduceYourInterestRates = 73,
-    AllOfTheAbove = 75,
-};
-
-export enum DebtType {
-    CreditCards = 77,
-    PersonalLoans = 79,
-    MedicalBills = 81,
+    ReduceYourMonthlyPayments = 80,
+    ConsolidateYourPayments = 81,
+    ReduceYourInterestRates = 82,
     AllOfTheAbove = 83,
 };
 
+export enum DebtType {
+    CreditCards = 84,
+    PersonalLoans = 85,
+    MedicalBills = 86,
+    AllOfTheAbove = 87,
+};
+
 export enum BehindPaymentsType {
-    YesMore60 = 85,
-    YesMore30 = 87,
-    No = 89,
+    YesMore60 = 88,
+    YesMore30 = 89,
+    No = 90,
 };
 
 export enum FallBehindReason {
     LossOfIncome = 91,
-    IncreasedCostOfLiving = 93,
-    UnexpectedExpenses = 95,
-    MedicalHardship = 97,
+    IncreasedCostOfLiving = 92,
+    UnexpectedExpenses = 93,
+    MedicalHardship = 94,
 };
 
 export enum Questions {
