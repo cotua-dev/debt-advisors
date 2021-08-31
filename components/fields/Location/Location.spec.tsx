a6b703441255a530b99f478db290c61d601fbffb
Jeffrey Berry <jeffreyberry93@gmail.com>
2021-08-31 09:28:47 -0700

    test(Location.spec.tsx): get test working

--- components/fields/Location/Location.spec.tsx
+++ components/fields/Location/Location.spec.tsx
@@ -1,10 +1,18 @@
 import { render, RenderResult } from '@testing-library/react';
 import { Location } from './index';
+import { LocationProps } from './Location.interfaces';
 
 describe('Location Component', (): void => {
     let renderResult: RenderResult;
+    const props: LocationProps = {
+        steps: [],
+        currentStep: 0,
+        zipCode: '',
+        setZipCode: () => {},
+        setUSState: () => {},
+    };
 
-    beforeAll((): RenderResult => renderResult = render(<Location/>));
+    beforeAll((): RenderResult => renderResult = render(<Location {...props}/>));
 
     test('renders', (): void => {
         const { container } = renderResult;
