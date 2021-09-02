501ef1423fc952275985b9aeee3f55f01015c20e
Jeffrey Berry <jeffreyberry93@gmail.com>
2021-09-02 11:01:46 -0700

    refactor(_document.tsx): use nextjs script tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,25 +1,26 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 class MyDocument extends Document {
     public render(): JSX.Element {
         return (
             <Html lang="en">
                 <Head>
-                    <script dangerouslySetInnerHTML={{__html: `
+                    <Script dangerouslySetInnerHTML={{__html: `
                     (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
                     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
                     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
                     })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM}');
-                    `}}></script>
-                    <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GTAG}`}></script>
-                    <script dangerouslySetInnerHTML={{__html: `
+                    `}}></Script>
+                    <Script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GTAG}`}></Script>
+                    <Script dangerouslySetInnerHTML={{__html: `
                     window.dataLayer = window.dataLayer || [];
                     function gtag(){dataLayer.push(arguments);}
                     gtag('js', new Date());
                     gtag('config', '${process.env.NEXT_PUBLIC_GTAG}');
-                    `}}></script>
-                    <script dangerouslySetInnerHTML={{__html:`
+                    `}}></Script>
+                    <Script dangerouslySetInnerHTML={{__html:`
                     !function(f,b,e,v,n,t,s)
                     {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
                     n.callMethod.apply(n,arguments):n.queue.push(arguments)};
@@ -30,7 +31,7 @@ class MyDocument extends Document {
                     'https://connect.facebook.net/en_US/fbevents.js');
                     fbq('init', '${process.env.NEXT_PUBLIC_FBQ}');
                     fbq('track', 'PageView');
-                    `}}></script>
+                    `}}></Script>
                     <link rel="preconnect" href="https://fonts.googleapis.com"/>
                     <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
                     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet"/>
@@ -39,7 +40,7 @@ class MyDocument extends Document {
                     <Main/>
                     <NextScript/>
                     <noscript><iframe src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM}`} height="0" width="0" style={{display:'none',visibility:'hidden'}}></iframe></noscript>
-                    <noscript><img height="1" width="1" style={{display:'none'}} src={`https://www.facebook.com/tr?id=${process.env.NEXT_PUBLIC_FBQ}&ev=PageView&noscript=1`}/></noscript>
+                    <noscript><img height="1" width="1" style={{display:'none'}} alt="Facebook Img" src={`https://www.facebook.com/tr?id=${process.env.NEXT_PUBLIC_FBQ}&ev=PageView&noscript=1`}/></noscript>
                 </body>
             </Html>
         );
