f80a978a15688a81a5f998dc968af5a07488040e
Jeffrey Berry <jeffreyberry93@gmail.com>
2021-09-02 12:12:49 -0700

    test(fields): add props to each field

--- components/fields/Name/Name.spec.tsx
+++ components/fields/Name/Name.spec.tsx
@@ -1,10 +1,19 @@
 import { render, RenderResult } from '@testing-library/react';
 import { Name } from './index';
+import { NameProps } from './Name.interfaces';
 
 describe('Name Component', (): void => {
     let renderResult: RenderResult;
+    const props: NameProps = {
+        steps: [],
+        currentStep: 0,
+        firstName: '',
+        lastName: '',
+        setFirstName: () => {},
+        setLastName: () => {},
+    };
 
-    beforeAll((): RenderResult => renderResult = render(<Name/>));
+    beforeAll((): RenderResult => renderResult = render(<Name {...props}/>));
 
     test('renders', (): void => {
         const { container } = renderResult;
--- components/fields/Phone/Phone.spec.tsx
+++ components/fields/Phone/Phone.spec.tsx
@@ -1,10 +1,18 @@
 import { render, RenderResult } from '@testing-library/react';
 import { Phone } from './index';
+import { PhoneProps } from './Phone.interfaces';
 
 describe('Phone Component', (): void => {
     let renderResult: RenderResult;
+    const props: PhoneProps = {
+        steps: [],
+        currentStep: 0,
+        setDisableNextButton: () => {},
+        phone: '',
+        setPhone: () => {},
+    };
 
-    beforeAll((): RenderResult => renderResult = render(<Phone/>));
+    beforeAll((): RenderResult => renderResult = render(<Phone {...props}/>));
 
     test('renders', (): void => {
         const { container } = renderResult;
