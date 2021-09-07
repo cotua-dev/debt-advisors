e17b1eee277b8ea831ff66a75a908287b86d34d9
Jeffrey Berry <jeffreyberry93@gmail.com>
2021-09-07 13:17:08 -0700

    wip(index.tsx): update data and step handling

--- components/Stepper/index.tsx
+++ components/Stepper/index.tsx
@@ -1,10 +1,13 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleRight, faAngleLeft, faCircleNotch } from '@fortawesome/free-solid-svg-icons';
 import styles from './Stepper.module.scss';
 import { ParsedStepperModel, Step, StepperModel } from './Stepper.interfaces';
 import { initialStepperModel, initialSteps } from './Stepper.initial';
-import { addBitrixContactDeal, parseCurrencyValue, parseModel, sendSMS, verifySMSCode } from './Stepper.utility';
+import {
+    addBitrixContactDeal, parseCurrencyValue, parseModel,
+    sendSMS, verifySMSCode, fetchStepIndex,
+} from './Stepper.utility';
 import { Questions } from './Stepper.enums';
 import { MultipleChoiceField } from '../fields/MultipleChoice';
 import { Currency } from '../fields/Currency';
@@ -13,6 +16,7 @@ import { Name } from '../fields/Name';
 import { Email } from '../fields/Email';
 import { Phone } from '../fields/Phone';
 import { Verify } from '../fields/Verify';
+import { MultipleChoiceValues } from './Stepper.types';
 
 export function Stepper(): JSX.Element {
     const [model, setModel] = useState(initialStepperModel);
@@ -25,6 +29,12 @@ export function Stepper(): JSX.Element {
     const [disableVerifyField, setDisableVerifyField] = useState(true);
     
     // Fields
+    const [userPurpose, setUserPurpose] = useState<null | MultipleChoiceValues>(null);
+    const [debtType, setDebtType] = useState<null | MultipleChoiceValues>(null);
+    const [behindPaymentsType, setBehindPaymentsType] = useState<null | MultipleChoiceValues>(null);
+    const [fallBehindReason, setFallBehindReason] = useState<null | MultipleChoiceValues>(null);
+    const [monthlyIncomeAmount, setMonthlyIncomeAmount] = useState('');
+    const [unsecuredDebtAmount, setUnsecuredDebtAmount] = useState('');
     const [zipCode, setZipCode] = useState('');
     const [firstName, setFirstName] = useState('');
     const [lastName, setLastName] = useState('');
@@ -123,6 +133,7 @@ export function Stepper(): JSX.Element {
         const { validity, question } = steps[currentStep];
 
         // Check if we passed the unsecured debt field
+        /*
         if (question === Questions.UnsecuredDebtAmount) {
             // Parse the unsecured debt amount into a number
             const unsecuredDebtAmountNumber: number = parseCurrencyValue(model.unsecuredDebtAmount);
@@ -133,6 +144,7 @@ export function Stepper(): JSX.Element {
                 window.location.href = `${window.location.origin}/dq`;
             }
         }
+        */
 
         // Check if we passed the zip code field
         if (question === Questions.ZipCode) {
@@ -193,26 +205,120 @@ export function Stepper(): JSX.Element {
         submission(parsedModel);
     }
 
+    useEffect(() => {
+        setSteps([
+            {
+                question: Questions.UserPurpose,
+                validity: false,
+                property: userPurpose,
+                dispatchMethod: setUserPurpose,
+                stepType: 'multiple-choice',
+                choices: [
+                    { label: 'Reduce your monthly payments', value: 0 },
+                    { label: 'Consolidate your payments', value: 1 },
+                    { label: 'Reduce your interest rates', value: 2 },
+                    { label: 'All of the above', value: 3 },
+                ],
+            },
+            {
+                question: Questions.DebtType,
+                validity: false,
+                property: debtType,
+                dispatchMethod: setDebtType,
+                stepType: 'multiple-choice',
+                choices: [
+                    { label: 'Credit cards', value: 0 },
+                    { label: 'Personal loans', value: 1 },
+                    { label: 'Medical bills', value: 2 },
+                    { label: 'All of the above', value: 3 },
+                ],
+            },
+            {
+                question: Questions.BehindPaymentsType,
+                validity: false,
+                property: behindPaymentsType,
+                dispatchMethod: setBehindPaymentsType,
+                stepType: 'multiple-choice',
+                choices: [
+                    { label: 'Yes - more than 60 days', value: 0 },
+                    { label: 'Yes - more than 30 days', value: 1 },
+                    { label: 'No', value: 2 },
+                ],
+            },
+            {
+                question: Questions.FallBehindReason,
+                validity: false,
+                property: fallBehindReason,
+                dispatchMethod: setFallBehindReason,
+                stepType: 'multiple-choice',
+                choices: [
+                    { label: 'Loss of income', value: 0 },
+                    { label: 'Increase in cost of living', value: 1 },
+                    { label: 'Unexpected expenses', value: 2 },
+                    { label: 'Medical hardship', value: 3 },
+                ],
+            },
+            {
+                question: Questions.MonthlyIncomeAmount,
+                validity: false,
+                property: monthlyIncomeAmount,
+                dispatchMethod: setMonthlyIncomeAmount,
+                stepType: 'currency',
+            },
+            {
+                question: Questions.UnsecuredDebtAmount,
+                validity: false,
+                property: unsecuredDebtAmount,
+                dispatchMethod: setUnsecuredDebtAmount,
+                stepType: 'currency',
+            },
+            {
+                question: Questions.ZipCode,
+                validity: false,
+                stepType: 'location',
+            },
+            {
+                question: Questions.Name,
+                validity: false,
+                stepType: 'name',
+            },
+            {
+                question: Questions.Email,
+                validity: false,
+                stepType: 'email',
+            },
+            {
+                question: Questions.Phone,
+                validity: false,
+                stepType: 'phone',
+            },
+            {
+                question: Questions.Code,
+                validity: false,
+                stepType: 'verify',
+            },
+        ]);
+    }, []);
+
     return (
         <section className={styles['debt-advisors-stepper']}>
-            <div>
-                <small>{`${currentStep + 1} of ${steps.length}`}</small>
-            </div>
+            <small>{`${currentStep + 1} of ${steps.length}`}</small>
             <div className={styles['steps-wrapper']}>
                 {steps.map((step: Step) => {
                     const stepIndex: number = steps.findIndex((thisStep: Step) => thisStep.question === step.question);
-                    const showStep: boolean = stepIndex === currentStep;
+                    let showStep: boolean = stepIndex === currentStep;
 
-                    return (
-                        <div key={step.question} className={`${styles['step']} ${showStep ? '' : styles['hidden']}`}>
+                    console.info('hi');
+
+                    return showStep && (
+                        <div key={step.question} className={`${styles['step']}`}>
                             <strong className={styles['question']}>{step.question}</strong>
                             {
                                 step.stepType === 'multiple-choice' &&
                                     <MultipleChoiceField
                                         choices={step.choices !== undefined ? step.choices : []}
                                         field={step.property}
-                                        setModel={setModel}
-                                        model={model}
+                                        setField={step.dispatchMethod ? step.dispatchMethod : () => {}}
                                         nextStep={nextStep}
                                         steps={steps}
                                         currentStep={currentStep}
@@ -222,8 +328,7 @@ export function Stepper(): JSX.Element {
                                 step.stepType === 'currency' &&
                                     <Currency
                                         field={step.property}
-                                        setModel={setModel}
-                                        model={model}
+                                        setField={step.dispatchMethod ? step.dispatchMethod : () => {}}
                                         steps={steps}
                                         currentStep={currentStep}
                                         setDisableNextButton={setDisableNextButton}
