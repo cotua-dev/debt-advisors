fb695a538419ea2925c6fb8726470da1ccf1576c
Jeffrey Berry <jeffreyberry93@gmail.com>
2021-09-07 13:09:55 -0700

    wip(shared.interfaces.ts): redo interface

--- components/fields/shared.interfaces.ts
+++ components/fields/shared.interfaces.ts
@@ -2,9 +2,9 @@ import { Dispatch, SetStateAction } from 'react';
 import { Step, StepperModel } from '../Stepper/Stepper.interfaces';
 
 export interface SharedFieldProps {
-    field: (keyof StepperModel)[];
-    setModel: Dispatch<SetStateAction<StepperModel>>;
-    model: StepperModel;
+    // field: keyof StepperModel;
+    setField: Dispatch<SetStateAction<any>>;
+    // model: StepperModel;
     steps: Step[];
     currentStep: number;
     setDisableNextButton?: Dispatch<SetStateAction<boolean>>;
