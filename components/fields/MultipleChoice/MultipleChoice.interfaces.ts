df4772c549aa27c87961ec8ab6f71c8c8292780a
Jeffrey Berry <jeffreyberry93@gmail.com>
2021-09-08 11:31:47 -0700

    chore(MultipleChoice.interfaces.ts): add setSteps dispatch method

--- components/fields/MultipleChoice/MultipleChoice.interfaces.ts
+++ components/fields/MultipleChoice/MultipleChoice.interfaces.ts
@@ -1,7 +1,9 @@
-import { MultipleChoice } from '../../Stepper/Stepper.interfaces';
+import { Dispatch, SetStateAction } from '.pnpm/@types+react@17.0.15/node_modules/@types/react';
+import { MultipleChoice, Step } from '../../Stepper/Stepper.interfaces';
 import { SharedFieldProps } from '../shared.interfaces';
 
 export interface MultipleChoiceFieldProps extends SharedFieldProps {
     choices: MultipleChoice[];
     nextStep: Function;
+    setSteps: Dispatch<SetStateAction<Step[]>>;
 };
