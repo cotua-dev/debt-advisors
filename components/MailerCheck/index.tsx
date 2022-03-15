f71bb729f7eb7ae0ad2ac674c06c0cf78cbd6ebe
Jeffrey Berry <jeffreyberry93@gmail.com>
2022-03-15 13:45:11 -0700

    refactor(index.tsx): update styling of reference id input fields

--- components/MailerCheck/index.tsx
+++ components/MailerCheck/index.tsx
@@ -64,15 +64,15 @@ export function MailerCheck(): JSX.Element {
                         <div className="block mb-4">
                             <input
                                 type="text"
-                                className="w-16 text-lg text-center p-1 mr-1 border border-1 rounded"
-                                maxLength={4}
+                                className="w-16 text-base text-center p-1 mr-1 border border-1 rounded"
+                                maxLength={5}
                                 onChange={(e: ChangeEvent<HTMLInputElement>) => setFirstField(e.target.value)}
                             />
                             {`–`}
                             <input
                                 type="text"
-                                className="w-16 text-lg text-center p-1 ml-1 border border-1 rounded"
-                                maxLength={5}
+                                className="w-20 text-base text-center p-1 ml-1 border border-1 rounded"
+                                maxLength={6}
                                 onChange={(e: ChangeEvent<HTMLInputElement>) => setSecondField(e.target.value)}
                             />
                         </div>
