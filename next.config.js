c3d6fe428ba23f6ca495137086e7e85d631d022e
Jeffrey Berry <jeffreyberry93@gmail.com>
2022-10-05 08:03:31 -0700

    fix(next.config.js): remove warning

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
-const withPWA = require('next-pwa');
 const runtimeCaching = require('next-pwa/cache');
 const production = process.env.NODE_ENV === 'production';
 
-const settings = {
-    reactStrictMode: true,
-    swcMinify: true,
-};
+const withPWA = require('next-pwa')({
+    dest: 'public',
+    runtimeCaching,
+});
 
-module.exports = production ? withPWA({ ...settings, pwa: { dest: 'public', runtimeCaching } }) : settings;
+module.exports = production ? withPWA() : {
+    reactStrictMode: true,
+};
