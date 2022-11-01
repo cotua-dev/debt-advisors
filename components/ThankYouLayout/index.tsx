732e9d337bf387feced6032311840bdd429d7c7d
Jeffrey Berry <jeffreyberry93@gmail.com>
2022-11-01 11:24:52 -0700

    feat(index.tsx): pass a conversion value to specify the value of a lead

--- components/ThankYouLayout/index.tsx
+++ components/ThankYouLayout/index.tsx
@@ -1,14 +1,23 @@
+import { useState } from 'react';
+import { useRouter } from 'next/router';
 import Script from 'next/script';
 import { ThankYouLayoutProps } from './ThankYouLayout.interfaces';
 
 export function ThankYouLayout({ children }: ThankYouLayoutProps): JSX.Element {
+    const router = useRouter();
+    const [conversionValue] = useState(router.pathname === '/thank-you' ? 100 : 10);
+
     return (
         <>
             <Script
                 strategy="afterInteractive"
                 id="google-tag-conversion"
                 dangerouslySetInnerHTML={{__html: `
-                    gtag('event', 'conversion', {'send_to': '${process.env.NEXT_PUBLIC_GTAG}/${process.env.NEXT_PUBLIC_GTM_THANK_YOU}'});
+                    gtag('event', 'conversion', {
+                        'send_to': '${process.env.NEXT_PUBLIC_GTAG}/${process.env.NEXT_PUBLIC_GTM_THANK_YOU}',
+                        'value': ${conversionValue},
+                        'currency': 'USD'
+                    });
                 `}}
             />
             <Script
