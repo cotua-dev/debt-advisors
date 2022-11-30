3024794c46f73c75ed04c7f9c2211b1a3307e3cf
Jeffrey Berry <jeffreyberry93@gmail.com>
2022-11-30 11:18:59 -0800

    feat(index.tsx): create the google tag conversion script in `useEffect` so we can grab the conversion value out of `localStorage`

--- components/ThankYouLayout/index.tsx
+++ components/ThankYouLayout/index.tsx
@@ -1,15 +1,43 @@
-import { useState } from 'react';
-import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 import Script from 'next/script';
 import { ThankYouLayoutProps } from './ThankYouLayout.interfaces';
 
 export function ThankYouLayout({ children }: ThankYouLayoutProps): JSX.Element {
-    const router = useRouter();
-    const [conversionValue] = useState(router.pathname === '/thank-you' ? 100 : 10);
+    /**
+     * We needed to pass the unsecured debt amount to the google tag conversion script
+     * so we could validate the value of each lead. I found that setting the passed
+     * debt amount in `localStorage` and then creating the script tag in `useEffect` to be
+     * the easiest method. This works because we force a refresh after the stepper finishes
+     * due to setting `window.location.href` instead of utilizing NextJS's `router.push`.
+     * Feel free to try other methods if a better one is found.
+     */
+    useEffect(() => {
+        const localStorageAmount: string | null = localStorage.getItem('amount');
+        if (localStorageAmount !== null) {
+            const bodyEl: HTMLBodyElement | null = document.querySelector('body');
+            let conversionScript: HTMLElement | null = document.getElementById('google-tag-conversion');
+
+            if (conversionScript === null) {
+                conversionScript = document.createElement('script');
+                conversionScript.id = 'google-tag-conversion';
+                conversionScript.append(`
+                    gtag('event', 'conversion', {
+                        'send_to': '${process.env.NEXT_PUBLIC_GTAG}/${process.env.NEXT_PUBLIC_GTM_THANK_YOU}',
+                        'value': ${Number(localStorageAmount)},
+                        'currency': 'USD'
+                    });
+                `);
+
+                if (bodyEl !== null) {
+                    bodyEl.append(conversionScript);
+                }
+            }
+        }
+    });
 
     return (
         <>
-            <Script
+            {/* <Script
                 strategy="afterInteractive"
                 id="google-tag-conversion"
                 dangerouslySetInnerHTML={{__html: `
@@ -19,7 +47,7 @@ export function ThankYouLayout({ children }: ThankYouLayoutProps): JSX.Element {
                         'currency': 'USD'
                     });
                 `}}
-            />
+            /> */}
             <Script
                 strategy="afterInteractive"
                 id="facebook-track-submit-application"
