8021259e39a2fe458fecf6e26f7729452a894b7d
Jeffrey Berry <jeffreyberry93@gmail.com>
2022-12-06 12:01:30 -0800

    feat(index.tsx): pass data to facebook and google

--- components/ThankYouLayout/index.tsx
+++ components/ThankYouLayout/index.tsx
@@ -1,8 +1,17 @@
 import { useEffect } from 'react';
-import Script from 'next/script';
+import { SHA256 } from 'crypto-js';
 import { ThankYouLayoutProps } from './ThankYouLayout.interfaces';
 
 export function ThankYouLayout({ children }: ThankYouLayoutProps): JSX.Element {
+    const parsePhoneNumber = (phone: string): string => {
+        let parsedPhoneNumber = '';
+
+        // Remove whitespace and '+'
+        parsedPhoneNumber = phone.split(' ').join('').replace('+', '');
+
+        return parsedPhoneNumber;
+    }
+
     /**
      * We needed to pass the unsecured debt amount to the google tag conversion script
      * so we could validate the value of each lead. I found that setting the passed
@@ -12,25 +21,82 @@ export function ThankYouLayout({ children }: ThankYouLayoutProps): JSX.Element {
      * Feel free to try other methods if a better one is found.
      */
     useEffect(() => {
-        const localStorageAmount: string | null = localStorage.getItem('amount');
-        if (localStorageAmount !== null) {
-            const bodyEl: HTMLBodyElement | null = document.querySelector('body');
-            let conversionScript: HTMLElement | null = document.getElementById('google-tag-conversion');
+        const bodyEl: HTMLBodyElement | null = document.querySelector('body');
 
-            if (conversionScript === null) {
-                conversionScript = document.createElement('script');
-                conversionScript.id = 'google-tag-conversion';
-                conversionScript.append(`
-                    gtag('event', 'conversion', {
-                        'send_to': '${process.env.NEXT_PUBLIC_GTAG}/${process.env.NEXT_PUBLIC_GTM_THANK_YOU}',
-                        'value': ${Number(localStorageAmount)},
-                        'currency': 'USD'
-                    });
-                `);
+        // Grab data from localStorage
+        const amount: string | null = localStorage.getItem('amount');
+        const email: string | null = localStorage.getItem('email');
+        const phone: string | null = localStorage.getItem('phone');
+        const firstName: string | null = localStorage.getItem('firstName');
+        const lastName: string | null = localStorage.getItem('lastName');
 
-                if (bodyEl !== null) {
+        // Make sure we have the body element
+        if (bodyEl !== null) {
+            // Only add the conversion script if we have an amount in localStorage
+            if (amount !== null) {
+                let conversionScript: HTMLElement | null = document.getElementById('google-tag-conversion');
+
+                if (conversionScript === null) {
+                    conversionScript = document.createElement('script');
+                    conversionScript.id = 'google-tag-conversion';
+                    conversionScript.append(`
+                        gtag('event', 'conversion', {
+                            'send_to': '${process.env.NEXT_PUBLIC_GTAG}/${process.env.NEXT_PUBLIC_GTM_THANK_YOU}',
+                            'value': ${Number(amount)},
+                            'currency': 'USD'
+                        });
+                    `);
+
                     bodyEl.append(conversionScript);
                 }
+            }
+
+            // Only add the pixel script if we have its lead data in localStorage
+            if (
+                email !== null &&
+                phone !== null &&
+                firstName !== null &&
+                lastName !== null
+            ) {
+                // let pixelScript: HTMLElement | null = document.getElementById('facebook-track-submit-application');
+
+                const parsedPhoneNumber: string = parsePhoneNumber(phone);
+                console.info({parsedPhoneNumber});
+
+                const emailHash = SHA256(email);
+                const phoneHash = SHA256(parsedPhoneNumber);
+                const firstNameHash = SHA256(firstName);
+                const lastNameHash = SHA256(lastName);
+
+                // Send data off to Facebook
+                (window as any).fbq('track', 'Lead', {
+                    em: emailHash.toString(),
+                    ph: phoneHash.toString(),
+                    fn: firstNameHash.toString(),
+                    ln: lastNameHash.toString(),
+                });
+
+                // if (pixelScript !== null) {
+                //     pixelScript = document.createElement('script');
+                //     pixelScript.id = 'facebook-track-submit-application';
+                //     pixelScript.append(`
+                //         fbq('track', 'SubmitApplication');
+                //         fbq('track', 'Lead', {
+                //             em: '${emailHash.toString()}',
+                //             ph: '${phoneHash.toString()}',
+                //             fn: '${firstNameHash.toString()}',
+                //             ln: '${lastNameHash.toString()}',
+                //         });
+                //     `);
+                // }
+
+                // Send data off to Google
+                (window as any).dataLayer.push({
+                    email: emailHash.toString(),
+                    phone_number: phoneHash.toString(),
+                    first_name: firstNameHash.toString(),
+                    last_name: lastNameHash.toString(),
+                });
             }
         }
     });
@@ -48,14 +114,19 @@ export function ThankYouLayout({ children }: ThankYouLayoutProps): JSX.Element {
                     });
                 `}}
             /> */}
-            <Script
+            {/* <Script
                 strategy="afterInteractive"
                 id="facebook-track-submit-application"
                 dangerouslySetInnerHTML={{__html: `
                     fbq('track', 'SubmitApplication');
-                    fbq('track', 'Lead');
+                    fbq('track', 'Lead', {
+                        em: '',
+                        ph: '',
+                        fn: '',
+                        ln: '',
+                    });
                 `}}
-            />
+            /> */}
             {children}
         </>
     );
