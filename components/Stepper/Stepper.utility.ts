7172be85c4d2ed00538bdf3f0341c8d359428ffb
Jeffrey Berry <jeffreyberry93@gmail.com>
2023-01-20 12:18:30 -0800

    chore(Stepper.utility.ts): stop executing server-side conversion requests

--- components/Stepper/Stepper.utility.ts
+++ components/Stepper/Stepper.utility.ts
@@ -218,8 +218,8 @@ export async function addBitrixContactDeal(data: ParsedStepperModel): Promise<Re
             body: JSON.stringify(data),
         });
 
-        await sendFacebookConversion(data);
-        await sendCotuaDAAConversion(data);
+        // await sendFacebookConversion(data);
+        // await sendCotuaDAAConversion(data);
 
         // Return the response object
         return response;
