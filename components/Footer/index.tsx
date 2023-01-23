ba318450a0d456261c844023587e1bd3332a29ca
Jeffrey Berry <jeffreyberry93@gmail.com>
2023-01-23 12:12:59 -0800

    wip(index.tsx): still need to figure out how to get the trustpilot widget rendering correctly

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,10 +1,14 @@
+import { useRouter } from 'next/router';
 import Link from 'next/link';
+import { v4 as uuidv4 } from 'uuid';
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 // import { faPhoneAlt, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import styles from './Footer.module.scss';
 import whiteLogo from '../../public/images/debt-advisors-white-logo.png';
 
 export function Footer(): JSX.Element {
+    const router = useRouter();
+    // cite: https://support.trustpilot.com/hc/en-us/articles/115011421468--Add-a-TrustBox-widget-to-a-single-page-application
     return (
         <footer className={styles['debt-advisors-footer']}>
             <div className={styles['footer-content-wrapper']}>
@@ -81,6 +85,22 @@ export function Footer(): JSX.Element {
                     {'Privacy Policy'}
                 </Link>
             </div>
+            {/* <div className="mt-4">
+                <div
+                    className="trustpilot-widget"
+                    data-locale="en-US"
+                    data-template-id="56278e9abfbbba0bdcd568bc"
+                    data-businessunit-id="63c8878df7ad8e71f3e6dae2"
+                    data-style-height="52px"
+                    data-style-width="100%"
+                >
+                    <a
+                        href="https://www.trustpilot.com/review/debtadvisorsofamerica.com"
+                        target="_blank"
+                        rel="noreferrer"
+                    >Trustpilot</a>
+                </div>
+            </div> */}
         </footer>
     );
 };
