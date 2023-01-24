83cfd670dbff8453c9f6e66aef075974add8e9c1
Jeffrey Berry <jeffreyberry93@gmail.com>
2023-01-24 09:27:18 -0800

    refactor(pages): only use the `ThankYouLayout` component

--- pages/ohio/thank-you.tsx
+++ pages/ohio/thank-you.tsx
@@ -1,22 +1,19 @@
 import Link from 'next/link';
-import { Layout } from '../../components/Layout';
 import { ThankYouLayout } from '../../components/ThankYouLayout';
 import styles from '../../styles/shared.module.scss';
 
 function ThankYouOhio(): JSX.Element {
     return (
-        <ThankYouLayout>
-            <Layout title="Thank You – Debt Advisors">
-                <section className={styles['thank-you']}>
-                    <div className={styles['content-wrapper']}>
-                        <h1>{`Thank you for registering!`}</h1>
-                        <p>{`A representative will contact you shortly`}</p>
-                        <Link href="/">
-                            {`Return to home page`}
-                        </Link>
-                    </div>
-                </section>
-            </Layout>
+        <ThankYouLayout title="Thank You – Debt Advisors">
+            <section className={styles['thank-you']}>
+                <div className={styles['content-wrapper']}>
+                    <h1>{`Thank you for registering!`}</h1>
+                    <p>{`A representative will contact you shortly`}</p>
+                    <Link href="/">
+                        {`Return to home page`}
+                    </Link>
+                </div>
+            </section>
         </ThankYouLayout>
     );
 }
--- pages/thanks.tsx
+++ pages/thanks.tsx
@@ -1,22 +1,19 @@
-import { Layout } from '../components/Layout';
 import { ThankYouLayout } from '../components/ThankYouLayout';
 import styles from '../styles/shared.module.scss';
 
 function ThankYouOne(): JSX.Element {
     return (
-        <ThankYouLayout>
-            <Layout title="Thank You – Debt Advisors">
-                <section className={styles['thank-you']}>
-                    <div className={styles['content-wrapper']}>
-                        <h1>{`Thank you for registering!`}</h1>
-                        <p>{`A representative will contact you shortly`}</p>
-                        <a
-                            className={styles['thank-you-button']}
-                            href="tel:+18442763691"
-                        >{`Click to Call`}</a>
-                    </div>
-                </section>
-            </Layout>
+        <ThankYouLayout title="Thank You – Debt Advisors">
+            <section className={styles['thank-you']}>
+                <div className={styles['content-wrapper']}>
+                    <h1>{`Thank you for registering!`}</h1>
+                    <p>{`A representative will contact you shortly`}</p>
+                    <a
+                        className={styles['thank-you-button']}
+                        href="tel:+18442763691"
+                    >{`Click to Call`}</a>
+                </div>
+            </section>
         </ThankYouLayout>
     );
 }
