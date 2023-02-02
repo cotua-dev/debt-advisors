7947ba090490461aa74b1959f9cb836d4043f3db
Jeffrey Berry <jeffreyberry93@gmail.com>
2023-02-02 09:55:27 -0800

    chore(shared): remove unused directory

--- shared/googleConversions.ts
+++
@@ -1,7 +0,0 @@
-export function conversion(amount: string | null) {
-    (window as any).gtag("event", "conversion", {
-        send_to: "AW-315822587/eO1NCPPzzPQCEPujzJYB",
-        value: Number(amount || 0),
-        currency: "USD",
-    });
-}
